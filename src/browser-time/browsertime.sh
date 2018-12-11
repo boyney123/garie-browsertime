69337347035171ef96173df128a3232c36ccef56
valentinab25 <valentinab25>
2018-12-11 17:15:59 +0200

    Add cleanup, get location for reports from docker

--- src/browser-time/browsertime.sh
+++ src/browser-time/browsertime.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 echo "Start getting data"
-echo "Saving reports into $REPORT_DIR/reports"
+
+docker_id=$( cat /proc/self/cgroup | head -n 1 | sed  's#.*/\([0-9a-fA-F]*\)$#\1#' )
+
+REPORTDIR=$(docker inspect $docker_id | grep :/usr/src/garie-browsertime/reports | awk -F'["|:]' '{print $2}')
+
+echo "Saving reports into $REPORTDIR"
 
 echo "Getting data for: $1"
 
-docker run --shm-size=1g -v $REPORT_DIR/reports:/browsertime sitespeedio/browsertime $1
+docker run --rm --shm-size=1g -v $REPORTDIR:/browsertime sitespeedio/browsertime $1
 
 echo "Finished getting data for: $1"
 
